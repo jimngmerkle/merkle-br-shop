bc5cc828d055f83ba2c56fd06442a83a100bda56
jimngmerkle <126862696+jimngmerkle@users.noreply.github.com>
2024-10-29 12:13:31 +0100

    Footer update

--- components/footer.tsx
+++ components/footer.tsx
@@ -2,7 +2,7 @@ export default function Footer() {
 	return (
 		<div className={'container mt-5 mb-4'}>
 			<footer className={'p-2 border-top'}>
-				This is footer.
+				This is my footer.
 			</footer>
 		</div>
 	);
